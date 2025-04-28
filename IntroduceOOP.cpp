d53cfe20f2a384075bc79a723117db23126134ff
sekar243 <kinasekin6602@gmail.com>
2025-04-28 19:12:19 +0700

    fariable introduce

--- IntroduceOOP.cpp
+++ IntroduceOOP.cpp
@@ -1,33 +1,3 @@
 #include <iostream>
 using namespace std;
 
-class Planet {
-    private :
-    float gravitasi;
-    public :
-    string nama;
-    float diameter;
-    
-    void displaydata() {
-        cout << "gravitasiku " << gravitasi << endl;
-        
-    }
-  void inputdata() {
-    cout << "input nama : ";
-    cin >> nama; 
-    cout << "ukuranku : ";
-    cin >> diameter;
-    cout << "gravitasiku : ";
-    cin >> gravitasi;
-  }
-    
-};
-int main()
-{
-    Planet venus ;
-    venus.inputdata();
-    cout << "namanya adalah " << venus.nama << endl;
-    cout << "diameter adalah " << venus.diameter << endl; 
-    
-    venus.displaydata();
-}
